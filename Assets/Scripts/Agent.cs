d27e22063217dc6391d903b4773d04c660766a05
Gavin Rens <gavinrens@gmail.com>
2022-09-12 12:05:42 +0200

    Update Agent.cs and cosmetic improvements

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,14 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
+
 
 public abstract class Agent
 {
     State currentState;
     Action currentAction;
-    static HashSet<Action> actions;
+    HashSet<Action> actions;
     static List<Observation> observations;
-    static protected List<State> states; 
-    
+    static protected List<State> states;
+
     public Agent()
     {
         currentState = null;  // must be initialized in the agent controller using this.InitializeAgentState()
@@ -26,22 +26,40 @@ public abstract class Agent
 
         InitializeAgentState();
     }
-    
+
     public State CurrentState { get { return currentState; } set { currentState = value; } }
 
     public Action CurrentAction { get { return currentAction; } set { currentAction = value; } }
 
-    public static HashSet<Action> Actions { get { return actions; } }
+    public HashSet<Action> Actions { get { return actions; } set { actions = value; } }
 
     public static List<Observation> Observations { get { return observations; } }
 
     public static List<State> States { get { return states; } }
 
+    /// <summary>
+    /// The state the agent expects to end up in if it executes the action in the current state
+    /// </summary>
+    /// <param name="action">The action executed</param>
+    /// <param name="state">The state in which the action was executed</param>
+    /// <returns>A successor state</returns>
     public abstract State GetNextState(Action action, State state);
 
+    /// <summary>
+    /// Generates the states that the agent can be in
+    /// </summary>
+    /// <returns>A list of agent states</returns>
     public abstract List<State> GenerateStates();
 
+    /// <summary>
+    /// Sets the state that the agent will start in
+    /// </summary>
     public abstract void InitializeAgentState();
 
+    /// <summary>
+    /// Defines when the agent will stop being active, i.e., defines the end of an episode. Should always return false if the agent does not stop.
+    /// </summary>
+    /// <param name="state">An 'end' state; where the agent stops or the episode ends</param>
+    /// <returns>true or false</returns>
     public abstract bool HasFinished(State state);
 }
--- Assets/Scripts/Planner_Interface.cs
+++ Assets/Scripts/Planner_Interface.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public interface Planner_Interface
 {
-    public Action SelectAction(State currentState, Agent agent = null);
+    public Action SelectAction(State currentState, );
 }
